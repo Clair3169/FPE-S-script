41a6ffc8b25dc4f5ed1c9550d51114b24523ee36
ClaireExploiting <yisusjj27@gmail.com>
2025-10-12 17:27:18 -0400

    Update FPE:s_esp_script.lua

--- FPE:s_esp_script.lua
+++ FPE:s_esp_script.lua
@@ -171,7 +171,7 @@ do
 	local States = {
 		Off = "rbxassetid://70491444431002",
 		On = "rbxassetid://139177094823080",
-		Lock = "rbxassetid://129593520417631",
+		Lock = "rbxassetid://110850184030204",
 		Lock2 = "rbxasset://SystemCursors/Cross"
 	}
 	local MaxLength = 900000
