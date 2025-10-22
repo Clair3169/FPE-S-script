31e27a4928e1c320a4b21b2c1760add9a7c7142f
ClaireExploiting <yisusjj27@gmail.com>
2025-10-22 08:13:30 -0400

    Create Delete_Areas.lua

---
+++ Delete_Areas.lua
@@ -0,0 +1,21 @@
+local workspace = game:GetService("Workspace")
+
+local dronesFolder = workspace:FindFirstChild("Drones")
+
+if dronesFolder then
+	dronesFolder:ClearAllChildren()
+end
+
+local areaFolder = workspace:FindFirstChild("Area")
+local leaderboardFolder = nil
+
+if areaFolder then
+	local mapFolder = areaFolder:FindFirstChild("Map")
+	if mapFolder then
+		leaderboardFolder = mapFolder:FindFirstChild("Leaderboard")
+	end
+end
+
+if leaderboardFolder then
+	leaderboardFolder:ClearAllChildren()
+end
