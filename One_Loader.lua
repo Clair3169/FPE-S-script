48fd65d6347fc0cb506278c2bc593725742f82d1
ClaireExploiting <yisusjj27@gmail.com>
2025-10-25 17:07:48 -0400

    Update One_Loader.lua

--- One_Loader.lua
+++ One_Loader.lua
@@ -11,8 +11,7 @@ local function new_loader()
     local self = setmetatable({}, Loader)
 
     self.scripts = {
-        {name = "Script FPE", url = "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Principal_Loader.lua"},
-        {name = "Owner", url = "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/SprintFake.lua"}
+        {name = "Script FPE", url = "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Principal_Loader.lua"}
     }
 
     self.loaded_urls = global_env.FPE_loaded_urls or {}
