462037c25b3439d256097924aab4c5cf01601f2f
ClaireExploiting <yisusjj27@gmail.com>
2025-11-10 01:02:24 -0400

    Create Notification_Walkspeed.lua

---
+++ Notification_Walkspeed.lua
@@ -0,0 +1,24 @@
+task.wait(2)
+
+local player = game.Players.LocalPlayer
+local StarterGui = game:GetService("StarterGui")
+
+local bindableFunction = Instance.new("BindableFunction")
+bindableFunction.OnInvoke = function(buttonClicked)
+	if buttonClicked == "Yes" then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Cframe_Walkspeed.lua", true))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Clair3169/FPE-S-script-Test/refs/heads/main/Notification_Warning.lua", true))()
+	elseif buttonClicked == "No" then
+		-- no se hace nada
+	end
+end
+
+StarterGui:SetCore("SendNotification", {
+	Title = "Hey again!",
+	Text = "Do you want to activate speed mode?",
+	Icon = "rbxassetid://97207642508375",
+	Duration = 20,
+	Callback = bindableFunction,
+	Button1 = "Yes",
+	Button2 = "No"
+})￼Enter
