124afdae5f09619a93b9cdae58e5965bc2494d09
ClaireExploiting <yisusjj27@gmail.com>
2025-11-13 00:23:34 -0400

    Update One_Loader.lua

--- One_Loader.lua
+++ One_Loader.lua
@@ -17,7 +17,8 @@ local urls = {
   "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Time.lua",
   "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Visual_Enraged.lua",
   "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Welcome_Script.lua",
-  "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Esp_Books.lua"
+  "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Esp_Books.lua",
+  "https://raw.githubusercontent.com/Clair3169/FPE-S-script/refs/heads/main/Anti_Camera.lua"
 }
 
 -- NOMBRE DE LA MARCA PARA RECORDAR QUE YA SE EJECUTÓ
